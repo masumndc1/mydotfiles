6f85bcc6575750ec943e8f64b13742a82caa4fac
Khabir Uddin <masumndc1@gmail.com>
2022-09-16 19:25:41 +0300

    python3 script for zshrc config

---
+++ set_zsh.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+from subprocess import check_call as call
+
+home = os.path.expanduser('~')
+pkgs = {'bat','fzf','ripgrep','exa'}
+
+def set_zsh(file):
+
+    location_zsh_conf = home + "/." + file
+    backup_zsh_conf = home + "/." + file + ".bk"
+
+    if os.path.exists(location_zsh_conf):
+        shutil.copy(location_zsh_conf, backup_zsh_conf)
+        shutil.copy(file, location_zsh_conf)
+    else:
+        shutil.copy(file, location_zsh_conf)
+
+
+def install_pkgs():
+    for pkg in pkgs:
+        if pkg == 'ripgrep':
+            pkg_path = "/usr/bin/rg"
+        else:
+            pkg_path = "/usr/bin/" + pkg
+        if not os.path.exists(pkg_path):
+            call("sudo zypper install -y {}".format(pkg), shell=True)
+
+def install_ohmyzsh():
+    if os.path.exists("/usr/bin/zypper"):
+        ohmyzsh_path = home + "/.oh-my-zsh"
+        if not os.path.exists(ohmyzsh_path):
+            call("curl -fsSL \
+                https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh \
+                | sh)", shell=True)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        file = sys.argv[1]
+        set_zsh(file)
+        install_pkgs()
+        install_ohmyzsh()
+    else:
+        print("set_zsh file_name")
