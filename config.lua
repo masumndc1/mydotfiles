51a3c714f7e5588aa046723378ad718cb1ce8f03
Khabir Uddin <khuddin@csc.fi>
2023-04-14 18:51:25 +0300

    lunarvim: gruvbox colorscheme

--- config.lua
+++ config.lua
@@ -11,7 +11,8 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "gruvbox"
+-- lvim.colorscheme = "lunar"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -182,6 +183,9 @@ lvim.plugins = {
   },
   {
     "mrjones2014/nvim-ts-rainbow"
+  },
+  {
+    "ellisonleao/gruvbox.nvim"
   },
 }
 
