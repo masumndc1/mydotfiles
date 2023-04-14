2cc1928dc2a19ac6b262411d93ec6f2424e20be9
Khabir Uddin <khuddin@csc.fi>
2023-04-14 18:42:14 +0300

    lunarvim: install noice

--- config.lua
+++ config.lua
@@ -168,6 +168,17 @@ lvim.plugins = {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
+  {
+    "folke/noice.nvim",
+    config = function()
+    require("noice").setup({
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
