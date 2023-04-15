6894a00dd8f75fe52062508850956d507324f209
Khabir Uddin <khuddin@csc.fi>
2023-04-15 18:50:15 +0300

    lunarvim: use tokyonight-moon as theme

--- config.lua
+++ config.lua
@@ -11,7 +11,8 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "tokyonight-moon"
+-- lvim.colorscheme = "lunar"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
