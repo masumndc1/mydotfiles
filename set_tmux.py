f25bce0a5eacf266c6cb348520f569a429b88fd3
khabiruddin <khabir.uddin@csc.fi>
2023-06-14 12:40:30 +0300

    tmux: set tpm plugins location automatically

--- set_tmux.py
+++ set_tmux.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import sys
+import subprocess
 
 
 def set_tmux(file):
@@ -10,12 +11,20 @@ def set_tmux(file):
     home = os.path.expanduser('~')
     location_tmux_conf = home + "/." + file
     backup_tmux_conf = home + "/." + file + ".bk"
+    tmux_plugins_location = home + "/.tmux/plugins/tpm"
 
     if os.path.exists(location_tmux_conf):
         shutil.copy(location_tmux_conf, backup_tmux_conf)
         shutil.copy(file, location_tmux_conf)
     else:
         shutil.copy(file, location_tmux_conf)
+
+    if not os.path.exists(tmux_plugins_location):
+        subprocess.check_output(
+            f"git clone https://github.com/tmux-plugins/tmp {tmux_plugins_location}",
+            shell=True)
+    else:
+        print("tpm location is set")
 
 
 if __name__ == '__main__':
