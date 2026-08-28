45bac1f55b6a3d43d37f74c44d4504ad4554ca93
Khabir Uddin <khuddin@csc.fi>
2026-08-28 16:19:47 +0300

    place fonts in freebsd

--- fonts.sh
+++ fonts.sh
@@ -7,3 +7,8 @@ if [[ $(uname) == "Linux" ]]; then
   echo -n "run this as root user"
   cp fonts/Terminus/*.ttf /usr/share/fonts
 fi
+
+elif [[ $(uname) == "FreeBSD" ]]; then
+  echo -n "run this as root user"
+  cp fonts/Terminus/*.ttf /usr/local/share/fonts
+fi
